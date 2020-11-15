1792aef5bb5dd2f558725728071ffdbd5b686acf
stephenwang0501 <zixuanwang0501@gmail.com>
2020-11-15 13:03:26 -0500

    Initialize an ASM project.

---
+++ using_registers.asm
@@ -0,0 +1,24 @@
+global _start
+
+section .text
+_start:
+	mov rdx, len
+	mov rsi, msg
+	mov rdi, 1
+	mov rax, 1
+	syscall
+
+	mov rdx, len_s
+	mov rsi, stars
+	mov rdi, 1
+	mov rax, 1
+	syscall
+
+	mov eax, 60
+	syscall
+
+section .data
+msg db 'Displaying 9 stars',13,10
+len equ $ - msg
+stars db '*********',13,10
+len_s equ $ - stars
